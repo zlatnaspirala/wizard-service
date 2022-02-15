0c17bab7e2d5092d4f2c315e460380a80b906621
zlatnaspirala <zlatnaspirala@gmail.com>
2022-02-15 23:12:04 +0100

    Update xy.js

--- xy.js
+++ xy.js
@@ -34,7 +34,7 @@ wss.on('connection', function connection(ws) {
             console.log("One wizard server on line .... with id -> ", getID);
             ws.info._type = "WIZARD-SERVER";
             ws.info.secret = getID;
-            fs.copyFile('secret.html', 'secret-wizard-' + getID + '.html', (err) => {
+            fs.copyFile('secret.html', './public/secret-wizard-' + getID + '.html', (err) => {
                 if (err) throw err;
                 console.log('Secret link created for ', getID);
             });
