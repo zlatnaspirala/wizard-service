b0af4217dbbb4cc1a4133836fcc7df4287dd90b8
zlatnaspirala <zlatnaspirala@gmail.com>
2022-02-16 20:51:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ var socketServer = new WebSocket.Server({port: WEBSOCKET_PORT, perMessageDeflate
 socketServer.connectionCount = 0;
 socketServer.on('connection', function(socket, upgradeReq) {
 	socketServer.connectionCount++;
-	console.log('New WebSocket Connection: test arg socket => ', (upgradeReq || socket.upgradeReq).headers);
-	console.log('New WebSocket Connection: test arg socket => ', (upgradeReq || socket.upgradeReq).socket.origin);
+	console.log('New WebSocket Connection: test1 arg socket => ', (upgradeReq || socket.upgradeReq).headers);
+	console.log('New WebSocket Connection: test2 arg socket => ', (upgradeReq || socket.upgradeReq).socket.origin);
 
 	console.log(
 		'New WebSocket Connection: ', 
